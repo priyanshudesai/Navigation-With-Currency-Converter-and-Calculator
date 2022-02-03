2e5b66fdd078a2ba27b92b8a7026c9b94ac681eb
hp <priyanshudesai9574@gmail.com>
2022-02-03 17:45:04 +0530

    Calculator Design Upgrade

--- app/src/main/java/com/pd/currencyconverter/ui/calculator/CalculatorFragment.kt
+++ app/src/main/java/com/pd/currencyconverter/ui/calculator/CalculatorFragment.kt
@@ -1,19 +1,24 @@
 package com.pd.currencyconverter.ui.calculator
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.pd.currencyconverter.R
 import com.pd.currencyconverter.databinding.FragmentCalculatorBinding
 
-class CalculatorFragment : Fragment(), TextWatcher {
+class CalculatorFragment : Fragment() {
 
     private var _binding: FragmentCalculatorBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var firstText : TextView
+    private lateinit var operatorText : TextView
+    private lateinit var secondText : TextView
+    private lateinit var answerText : TextView
+    private var operatorSet = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,28 +29,101 @@ class CalculatorFragment : Fragment(), TextWatcher {
         _binding = FragmentCalculatorBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        binding.btPlus.setOnClickListener {
-            binding.tvShowOperator.text = getString(R.string.plus)
-            calculate(getString(R.string.plus))
+        //result screen
+        firstText = binding.resultLayoutCalculator.tvFirstCalculator
+        operatorText = binding.resultLayoutCalculator.tvOperatorCalculator
+        secondText = binding.resultLayoutCalculator.tvSecondCalculator
+        answerText = binding.resultLayoutCalculator.tvAnswerCalculator
+
+        //Operands
+        binding.btnDot.setOnClickListener {
+            if (secondText.text.isNotEmpty() && !secondText.text.contains(getString(R.string.dot))){
+                setTextData(getString(R.string.dot))
+            } else if (firstText.text.isNotEmpty() && !firstText.text.contains(getString(R.string.dot)) && !operatorSet){
+                setTextData(getString(R.string.dot))
+            } else if (firstText.text.isEmpty()){
+                firstText.text = "0."
+            } else if (operatorSet && secondText.text.isEmpty()){
+                secondText.text = "0."
+            }
         }
-        binding.btMinus.setOnClickListener {
-            binding.tvShowOperator.text = getString(R.string.minus)
-            calculate(getString(R.string.minus))
+        binding.btnZero.setOnClickListener {
+            setTextData(getString(R.string._0))
         }
-        binding.btMultiplication.setOnClickListener {
-            binding.tvShowOperator.text = getString(R.string.multiply)
-            calculate(getString(R.string.multiply))
+        binding.btnOne.setOnClickListener {
+            setTextData(getString(R.string._1))
         }
-        binding.btDivision.setOnClickListener {
-            binding.tvShowOperator.text = getString(R.string.divide)
-            calculate(getString(R.string.divide))
+        binding.btnTwo.setOnClickListener {
+            setTextData(getString(R.string._2))
         }
-        binding.btModulus.setOnClickListener {
-            binding.tvShowOperator.text = getString(R.string.modulus)
-            calculate(getString(R.string.modulus))
+        binding.btnThree.setOnClickListener {
+            setTextData(getString(R.string._3))
         }
-        binding.etFirstValue.addTextChangedListener(this)
-        binding.etSecondValue.addTextChangedListener(this)
+        binding.btnFour.setOnClickListener {
+            setTextData(getString(R.string._4))
+        }
+        binding.btnFive.setOnClickListener {
+            setTextData(getString(R.string._5))
+        }
+        binding.btnSix.setOnClickListener {
+            setTextData(getString(R.string._6))
+        }
+        binding.btnSeven.setOnClickListener {
+            setTextData(getString(R.string._7))
+        }
+        binding.btnEight.setOnClickListener {
+            setTextData(getString(R.string._8))
+        }
+        binding.btnNine.setOnClickListener {
+            setTextData(getString(R.string._9))
+        }
+
+        //Operators
+        binding.btnPlus.setOnClickListener {
+            setOperatorData(getString(R.string.plus))
+        }
+        binding.btnMinus.setOnClickListener {
+            setOperatorData(getString(R.string.minus))
+        }
+        binding.btnMultiple.setOnClickListener {
+            setOperatorData(getString(R.string.multiply))
+        }
+        binding.btnDivide.setOnClickListener {
+            setOperatorData(getString(R.string.divide))
+        }
+        binding.btnModulus.setOnClickListener {
+            setOperatorData(getString(R.string.modulus))
+        }
+
+        //Equals
+        binding.btnEqual.setOnClickListener {
+            if (answerText.text.isNotEmpty()){
+                firstText.text = answerText.text
+                operatorSet = false
+                operatorText.text = ""
+                secondText.text = ""
+                answerText.text = ""
+            } else equalToCalculate()
+        }
+        
+        //Clear functions
+        binding.btnClear.setOnClickListener {
+            operatorSet = false
+            operatorText.text = ""
+            firstText.text = ""
+            secondText.text = ""
+            answerText.text = ""
+        }
+        binding.btnBack.setOnClickListener {
+            if (secondText.text.isNotEmpty()){
+                secondText.text = secondText.text.toString().dropLast(1)
+                answerText.text = ""
+            } else if (firstText.text.isNotEmpty()){
+                operatorSet = false
+                operatorText.text = ""
+                firstText.text = firstText.text.toString().dropLast(1)
+            }
+        }
 
         return root
     }
@@ -55,30 +133,58 @@ class CalculatorFragment : Fragment(), TextWatcher {
         _binding = null
     }
 
-    private fun calculate(operator: String) {
-        if (binding.etFirstValue.text.isNotEmpty() && binding.etSecondValue.text.isNotEmpty()) {
-            val firstValue = binding.etFirstValue.text.toString().toDouble()
-            val secondValue = binding.etSecondValue.text.toString().toDouble()
-            when (operator) {
-                getString(R.string.plus) -> binding.tvAnswer.text =
-                    (firstValue + secondValue).toString()
-                getString(R.string.minus) -> binding.tvAnswer.text =
-                    (firstValue - secondValue).toString()
-                getString(R.string.multiply) -> binding.tvAnswer.text =
-                    (firstValue * secondValue).toString()
-                getString(R.string.divide) -> binding.tvAnswer.text =
-                    (firstValue / secondValue).toString()
-                getString(R.string.modulus) -> binding.tvAnswer.text =
-                    (firstValue % secondValue).toString()
+    private fun setTextData(operand : String) {
+        if (!operatorSet) {
+            firstText.text = firstText.text.toString().plus(operand)
+        } else {
+            secondText.text = secondText.text.toString().plus(operand)
+            equalToCalculate()
+        }
+    }
+
+    private fun setOperatorData(operator: String){
+        if (firstText.text.isNotEmpty()) {
+            if (answerText.text.isNotEmpty()){
+                firstText.text = answerText.text
+                operatorText.text = operator
+                secondText.text = ""
+                answerText.text = ""
+            } else if (secondText.text.isNotEmpty()) {
+                equalToCalculate()
+            } else {
+                operatorSet = true
+                operatorText.text = operator
             }
         }
     }
 
-    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
-    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-        calculate(binding.tvShowOperator.text.toString())
+    private fun equalToCalculate(){
+        if (firstText.text.isNotEmpty() && secondText.text.isNotEmpty()){
+            val firstNumber = firstText.text.toString().toDouble()
+            val secondNumber = secondText.text.toString().toDouble()
+            val operator = operatorText.text.toString()
+            var result = 0.0
+            when (operator) {
+                getString(R.string.plus) -> {
+                    result = firstNumber + secondNumber
+                }
+                getString(R.string.minus) -> {
+                    result = firstNumber - secondNumber
+                }
+                getString(R.string.multiply) -> {
+                    result = firstNumber * secondNumber
+                }
+                getString(R.string.divide) -> {
+                    result = firstNumber / secondNumber
+                }
+                getString(R.string.modulus) -> {
+                    result = firstNumber % secondNumber
+                }
+            }
+            if (result.toString().endsWith(".0")){
+                result.toString().subSequence(0,result.toString().length-2)
+            }
+            answerText.text = result.toString()
+        }
     }
-
-    override fun afterTextChanged(p0: Editable?) {}
 }
